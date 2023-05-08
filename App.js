a8144a2da5c57b8dbf788acedfc6fff6a4dd8235
nahnahAJ <nahnahahjuwah@gmail.com>
2023-05-08 10:02:11 +0000

    Install linters and fix errors

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+const backgroundColor = '#fff';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello, world!</Text>
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor,
     alignItems: 'center',
     justifyContent: 'center',
   },
