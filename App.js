826ad04187812740cab4b9dba3e52aab6877e4a0
Felicia G. Awuah <nahnahahjuwah@gmail.com>
2023-05-20 04:27:02 +0000

    Make some changes to App.js

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MainScreen from './screens/MainScreen';
+// import MainScreen from './screens/MainScreen';
 import SpreadsheetScreen from './screens/SpreadsheetScreen';
 import SearchScreen from './screens/SearchScreen';
 import AddPropertyScreen from './screens/AddPropertyScreen';
+import HomePageScreen from './screens/HomePage';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={MainScreen} options={{ title: 'Spreadsheets' }} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator name="Home" component={HomePageScreen} options={{ title: "Homepage" }} />
+        {/* <Stack.Screen name="Main" component={MainScreen} options={{ title: 'Spreadsheets' }} /> */}
         <Stack.Screen name="Spreadsheet" component={SpreadsheetScreen} />
         <Stack.Screen name="Search" component={SearchScreen} />
         <Stack.Screen name="AddProperty" component={AddPropertyScreen} />
