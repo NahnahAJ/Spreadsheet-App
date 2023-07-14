5f8774419d6b94c15e34c8ff9c6ba792307e8314
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-14 15:33:29 +0000

    Install required dependencies

--- my-app/App.js
+++ my-app/App.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
+import SignIn from './src/screens/SignIn';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Sign In" component={SignIn} />
       </Stack.Navigator>
     </NavigationContainer>
   );
