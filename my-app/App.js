1faa5aea2c8093516fa53edca168d682102f1477
nahnahAJ <nahnahahjuwah@gmail.com>
2023-07-24 15:53:29 +0000

    Import new screens

--- my-app/App.js
+++ my-app/App.js
@@ -4,7 +4,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
-import SignIn from './src/screens/SignIn';
+import SignupScreen from './src/screens/SignupScreen';
+import AddProperty from './src/screens/AddProperty';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +15,9 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Sign In" component={SignIn} />
+        <Stack.Screen name="Sign Up" component={SignupScreen} />
+        <Stack.Screen name="Add Property" component={AddProperty} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
